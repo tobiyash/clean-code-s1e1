c9d9c5640b60573e2a8f3939c0179f9257436674
Anton <ta.sphinx@gmail.com>
2023-04-26 19:05:44 +0300

    refactor: update style.css and index.html according to the rules 3.4, 3.6, 3.7 in html-and-css.md

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className = "li"
     label.innerText=taskString;
     label.className='task label';
 
